7e479f5af1a108e369e6e3a94bd7f79ca09a578e
Jorge Zarzuela <j.zarzuela@hotmail.com>
2026-03-10 22:48:47 -0400

    Se agrega Form Usuarios

---
+++ Tickets/UsuarioDAL.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tickets
+{
+    internal class UsuarioDAL
+    {
+        Conexion conexion = new Conexion();
+
+        public void GuardarUsuario(string nombre, string email, string c, string correo)
+        {
+            SqlCommand cmd = new SqlCommand("sp_RegistrarUsuario", conexion.AbrirConexion());
+
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@nombre", nombre);
+            cmd.Parameters.AddWithValue("@apellido", apellido);
+            cmd.Parameters.AddWithValue("@cedula", cedula);
+            cmd.Parameters.AddWithValue("@correo", correo);
+
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
+}
